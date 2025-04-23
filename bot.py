7154ae57b192cf7bb39470d911aff9cfded7c20c
vlad tsilyna <vlad555vlad@gmail.com>
2025-04-23 19:29:11 +0300

    Fourth Lesson - add profile.

--- bot.py
+++ bot.py
@@ -92,6 +92,46 @@ async def message_button(update, context):
     await wait_message.edit_text(answer)
 
 
+async def profile(update, context):
+    dialog.mode = "profile"
+    msg = load_message("profile")
+    await send_photo(update, context, "profile")
+    await send_text(update, context, msg)
+
+    dialog.user.clear()
+    dialog.counter = 0
+
+    await send_text(update, context, "Скільки Вам років?")
+
+
+
+async def profile_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+
+    if dialog.counter == 1:
+        dialog.user["age"] = text
+        await send_text(update, context, "Ким ви працюєте?")
+    elif dialog.counter == 2:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Яку ви маєте хобі?")
+    elif dialog.counter == 3:
+        dialog.user["hobby"] = text
+        await send_text(update, context, "Що Вам НЕ подобається в людях?")
+    elif dialog.counter == 4:
+        dialog.user["annoys"] = text
+        await send_text(update, context, "Мета знайомства?")
+    elif dialog.counter == 5:
+        dialog.user["goals"] = text
+
+        prompt = load_prompt("profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+
+        wait_message = await send_text(update, context, "ChatGPT \uD83E\uDDE0 генерує Ваш профіль! Зачекайте......")
+        answer = await chatgpt.send_question(prompt, user_info)
+        await wait_message.edit_text(answer)
+
+
 async def hello(update, context):
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
@@ -99,11 +139,15 @@ async def hello(update, context):
         await date_dialog(update, context)
     elif dialog.mode == "message":
         await message_dialog(update, context)
+    elif dialog.mode == "profile":
+        await profile_dialog(update, context)
 
 
 dialog = Dialog()
 dialog.mode = "None"
 dialog.list = []
+dialog.user = {}
+dialog.counter = 0
 
 chatgpt = ChatGptService(
     token="gpt:AwXimzQXBV8upnlD2Fs0exPWVNTK9gmJGa_-erhzfPL1q5ldKc7zlb9PU53QlMYgrXBnjxK1uVJFkblB3TVSZ57z3Zm705v8y2sd9IG54evj_wDJ-zeNy_W7xT6lcYEGbp9qD9IbKvgZ5Ks22k_pKNEe74Sd")
@@ -113,6 +157,7 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("date", date))
 app.add_handler(CommandHandler("message", message))
 app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("profile", profile))
 app.add_handler(CallbackQueryHandler(date_button, pattern="date_.*"))
 app.add_handler(CallbackQueryHandler(message_button, pattern="message_.*"))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
