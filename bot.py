a5684a23640b394f630bad755b5088401cc370a9
vlad tsilyna <vlad555vlad@gmail.com>
2025-04-23 11:15:20 +0300

    Third Lesson finished. All works good!

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from util import *
 
 TOKEN = "7085486431:AAFuvGIUJ7_ueMU8EzClrxED9p11yYTTLps"
 
+
 async def start(update, context):
     dialog.mode = "None"
     msg = load_message("main")
@@ -21,10 +22,12 @@ async def start(update, context):
         "gpt": "Задати питання ChatGPT \uD83E\uDDE0"
     })
 
+
 async def gpt(update, context):
     dialog.mode = "gpt"
     await send_photo(update, context, "gpt")
     await send_text(update, context, load_message("gpt"))
+
 
 async def gpt_dialog(update, context):
     text = update.message.text
@@ -32,17 +35,85 @@ async def gpt_dialog(update, context):
     answer = await chatgpt.send_question(prompt, text)
     await send_text(update, context, answer)
 
+
+async def date(update, context):
+    dialog.mode = "date"
+    msg = load_message("date")
+    await send_photo(update, context, "date")
+    await send_text_buttons(update, context, msg, {
+        "date_grande": "Аріана Гранде",
+        "date_robbie": "Марго Роббі",
+        "date_zendaya": "Зендея",
+        "date_gosling": "Райан Гослінг",
+        "date_hardy": "Том Харді"
+    })
+
+
+async def date_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+    await send_photo(update, context, query)
+    await send_text(update, context,
+                    "Гарний вибір! \uD83D\uDE05 Ваше завдання запросити дівчину або хлопця на побачення за 5 повідомлень!💝")
+    prompt = load_prompt(query)
+    chatgpt.set_prompt(prompt)
+
+
+async def date_dialog(update, context):
+    text = update.message.text
+    wait_message = await send_text(update, context, "Набирає .....")
+    answer = await chatgpt.add_message(text)
+    await wait_message.edit_text(answer)
+
+
+async def message(update, context):
+    dialog.mode = "message"
+    msg = load_message("message")
+    await send_photo(update, context, "message")
+    await send_text_buttons(update, context, msg, {
+        "message_next": "Написати повідомлення",
+        "message_date": "Запросити на побачення"
+    })
+    dialog.list.clear()
+
+
+async def message_dialog(update, context):
+    text = update.message.text
+    dialog.list.append(text)
+
+
+async def message_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+    prompt = load_prompt(query)
+    user_chat_history = "\n\n".join(dialog.list)
+    wait_message = await send_text(update, context, "Розмірковую над варіантами .......")
+    answer = await chatgpt.send_question(prompt, user_chat_history)
+    await wait_message.edit_text(answer)
+
+
 async def hello(update, context):
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
+    elif dialog.mode == "date":
+        await date_dialog(update, context)
+    elif dialog.mode == "message":
+        await message_dialog(update, context)
+
 
 dialog = Dialog()
 dialog.mode = "None"
+dialog.list = []
 
-chatgpt = ChatGptService(token="gpt:AwXimzQXBV8upnlD2Fs0exPWVNTK9gmJGa_-erhzfPL1q5ldKc7zlb9PU53QlMYgrXBnjxK1uVJFkblB3TVSZ57z3Zm705v8y2sd9IG54evj_wDJ-zeNy_W7xT6lcYEGbp9qD9IbKvgZ5Ks22k_pKNEe74Sd")
+chatgpt = ChatGptService(
+    token="gpt:AwXimzQXBV8upnlD2Fs0exPWVNTK9gmJGa_-erhzfPL1q5ldKc7zlb9PU53QlMYgrXBnjxK1uVJFkblB3TVSZ57z3Zm705v8y2sd9IG54evj_wDJ-zeNy_W7xT6lcYEGbp9qD9IbKvgZ5Ks22k_pKNEe74Sd")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("date", date))
+app.add_handler(CommandHandler("message", message))
 app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CallbackQueryHandler(date_button, pattern="date_.*"))
+app.add_handler(CallbackQueryHandler(message_button, pattern="message_.*"))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.run_polling()
